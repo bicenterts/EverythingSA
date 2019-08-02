24f710cbfc3a9e61c163d89b291c35e4c76a4c7a
Kyle Mabaso <kyle@insytive.co.za>
2019-08-02 15:33:45 +0200

    Initial commit of the Everything South Africa app.

---
+++ app/src/main/java/com/example/everythingsa/AddEvent.java
@@ -0,0 +1,76 @@
+package com.example.everythingsa;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("Events");
+        eventTitle = (EditText) findViewById(R.id.eventTitle);
+        eventEntranceFee = (EditText) findViewById(R.id.eventEntranceFee);
+        eventDescription = (EditText) findViewById(R.id.eventDescription);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    EditText eventTitle;
+    EditText eventEntranceFee;
+    EditText eventDescription;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveEvent();
+                Toast.makeText(this, "Event Saved", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void saveEvent() {
+        String title = eventTitle.getText().toString();
+        String description = eventDescription.getText().toString();
+        String price = eventEntranceFee.getText().toString();
+        EventsClass event = new EventsClass(title, description, price, "");
+        mDatabaseReference.push().setValue(event);
+    }
+
+    public void clean() {
+        eventTitle.setText("");
+        eventTitle.requestFocus();
+        eventDescription.setText("");
+        eventEntranceFee.setText("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/everythingsa/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.everythingsa;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button eventsBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        eventsBtn = (Button) findViewById(R.id.eventsBtn);
+        eventsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddEvent();
+            }
+        });
+    }
+
+    private void openAddEvent() {
+        Intent intent = new Intent(this, AddEvent.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
